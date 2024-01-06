68b836b9c2171e8af4e0897920b842a2872f0018
Sakshi Jadaun <sakshijadaun080@gmail.com>
2024-01-06 21:32:17 +0530

    start add js

---
+++ index.js
@@ -0,0 +1,33 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGamebtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPositions = [
+
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+
+];
+
+//function to initialize the game
+function initGame(){
+
+    currentPlayer = "x";
+    gameGrid = ["","","","","","","","",""];
+    newGamebtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+
+}
+
+initGame();
+
+//wvent
